9361801cb9a6265966541318ab818bd70d49a0b6
Michael Stypa <michael@stypa.info>
2019-08-12 17:57:08 +0200

    fix timezone handling and fix import error checking

--- phimport.py
+++ phimport.py
@@ -1,8 +1,13 @@
 import influxdbconfig as cfg
 import sys
 import csv
+import os
 import time
 from influxdb import InfluxDBClient
+
+os.environ['TZ'] = 'UTC'
+time.tzset()
+print('Current timezone is', time.tzname)
 
 client = InfluxDBClient(host=cfg.influx['host'], port=cfg.influx['port'], username=cfg.influx['user'], password=cfg.influx['password'], ssl=True, verify_ssl=True)
 
@@ -22,16 +27,16 @@ with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     headers = next(csvreader, None)
     for row in csvreader:
-        eventid = row[0]
-        eventdate = row[1]
-        eventhours = float(row[2])
-        eventorg = row[3]
-        parsedeventdate = int(time.mktime(time.strptime(eventdate, '%Y-%m-%d')) * 1000000000)
-        if eventid == "" or eventdate == "" or eventhours == "" or eventorg == "":
+        if row[0] == "" or row[1] == "" or row[2] == "" or row[3] == "":
             print("illegal entry: ", end='')
             print(row)
             ignored+=1
         else:
+            eventid = row[0]
+            eventdate = row[1]
+            eventhours = float(row[2])
+            eventorg = row[3]
+            parsedeventdate = int(time.mktime(time.strptime(eventdate, '%Y-%m-%d')) * 1000000000)
             added+=1
             if (parsedeventdate, eventorg) in entries:
                 entries[(parsedeventdate, eventorg)]['fields']['hours'] += eventhours
